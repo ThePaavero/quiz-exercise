ec632d58183dec34db5859d9d0af6a18e7ecc8d3
Pekka <nospam@astudios.org>
2017-01-15 23:52:11 +0200

    Choice array is shuffled

--- cliApp/index.js
+++ cliApp/index.js
@@ -53,7 +53,7 @@ const promptUser = (q) => {
 const formatChoices = (q) => {
   const choices = q.incorrectAnswers
   choices.push(q.correctAnswer)
-  return choices
+  return shuffleArray(choices)
 }
 
 const countDocumentAmount = () => {
@@ -69,6 +69,16 @@ const run = () => {
     amountOfQuestions = count
     doRound()
   })
+}
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array;
 }
 
 run()
