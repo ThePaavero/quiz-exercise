4af4f64fefd6d17166129ac14b5c923dc52b43e1
Pekka <nospam@astudios.org>
2017-01-15 23:31:20 +0200

    Getting random Qs

--- cliApp/index.js
+++ cliApp/index.js
@@ -12,17 +12,41 @@ const state = {
   }
 }
 
+let amountOfQuestions = 0
+
 const db = new Datastore({
   filename: '../data',
   autoload: true
 })
 
 const getRandomQuestion = () => {
+  return new Promise((resolve) => {
+    const index = Math.random() * amountOfQuestions
+    db.findOne({}).skip(index).exec((err, doc) => {
+      resolve(doc)
+    })
+  })
+}
 
+const doRound = () => {
+  getRandomQuestion().then((q) => {
+    console.log(q)
+  })
+}
+
+const countDocumentAmount = () => {
+  return new Promise((resolve) => {
+    amountOfQuestions = db.count({}, (err, count) => {
+      resolve(count)
+    });
+  })
 }
 
 const run = () => {
-  // ...
+  countDocumentAmount().then((count) => {
+    amountOfQuestions = count
+    doRound()
+  })
 }
 
 run()
